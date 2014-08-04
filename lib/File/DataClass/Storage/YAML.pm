c22506d4d606c829651483d8c812691c09f21e4c
pjfl <github@roxsoft.co.uk>
2014-08-04 14:38:52 +0100

    0.1 Replaced namespace::sweep with autoclean

--- lib/File/DataClass/Storage/YAML.pm
+++ lib/File/DataClass/Storage/YAML.pm
@@ -1,8 +1,8 @@
 package File::DataClass::Storage::YAML;
 
 use 5.01;
-use namespace::sweep;
-use version; our $VERSION = qv( sprintf '0.1.%d', q$Rev: 3 $ =~ /\d+/gmx );
+use namespace::autoclean;
+use version; our $VERSION = qv( sprintf '0.1.%d', q$Rev: 4 $ =~ /\d+/gmx );
 
 use Moo;
 use MooX::Augment -class;
